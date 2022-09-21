924d332d4be3c77a374c9ed4bf153b5588b9008e
Emir Takayama <ngemir@gmail.com>
2022-09-21 01:42:18 -0300

    funçao imprimir
    o any no array alí está para corrigir

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -5,6 +5,7 @@ import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { NegociacoesService } from '../services/negociacoes-service.js';
+import { imprimir } from '../utils/imprimir.js';
 import { MensagemView } from '../views/mensagem-view.js';
 import { NegociacoesView } from '../views/negociacoes-view.js';
 
@@ -43,6 +44,9 @@ export class NegociacaoController {
         }
 
         this.negociacoes.adiciona(negociacao);
+
+        imprimir(negociacao, this.negociacoes);
+
         this.limparFormulario();
         this.atualizaView();
     }
